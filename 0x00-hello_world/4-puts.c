2f4415835a3a3a1be825df749eaacd7076a0b7a3
Shatudanjuma <maryaishatudanjuma@gmail.com>
2023-07-13 14:28:01 -0700

    4-puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *
+ * main-Main function
+ * return-returns,if successful
+ */
+int main(void)
+{
+     puts("\"Programming is like building a multilingual puzzle,");
+     return (0);
+}     

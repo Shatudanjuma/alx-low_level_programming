e018e79d02c6968af3b3fdb8742f5da64cce228a
Shatudanjuma <maryaishatudanjuma@gmail.com>
2023-07-16 09:33:23 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+# include<stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int d, s;
+
+	for (d = 49; s <= 56; s++)
+	{
+		for (d = 49; s <= 57; s++)
+		{
+			if (s > d)
+			{
+				putchar(d);
+				putchar(s);
+				if (d != 56 || s != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}

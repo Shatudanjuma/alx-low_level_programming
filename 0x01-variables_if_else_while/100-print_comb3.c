b11ab320169c2a8c05fece3187710a3b5d0ffbe7
Shatudanjuma <maryaishatudanjuma@gmail.com>
2023-07-16 11:18:54 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,7 +10,6 @@ int main(void)
 {
 	int digit1;
 	int digit2;
-
 	for (digit1 = 0x30; digit1 <= 0x39; digit1++)
 	{
 		for (digit2 = 0x30; digit2 <= 0x39; digit2++)
@@ -19,15 +18,14 @@ int main(void)
 			{
 				putchar(digit1);
 				putchar(digit2);
-
-				if ((digit1 >= 0x30 && digit1 < 0x38) && digit2 <= 0x39)		{
+				if ((digit1 >= 0x30 && digit1 < 0x38) && digit2 <= 0x39)
+				{
 					putchar(',');
 					putchar(' ');
 				}
 			}
 		}
 	}
-
 	putchar('\n');
 	return (0);
 }

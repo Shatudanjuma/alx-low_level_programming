299dffd4fbe5588232aa8d01ed5be85c6eae204a
Shatudanjuma <maryaishatudanjuma@gmail.com>
2023-08-04 14:24:48 -0700

    static libraries

---
+++ 0x09-static_libraries/1-strncat.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strncat - function to concatenate n bytes of two strings
+ * @dest: pointer destination
+ * @src: pointer source
+ * @n: number of byte
+ * Return: void
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int len = strlen(dest);
+	int i;
+
+	for (i = 0 ; i < n && *src != '\0' ; i++)
+	{
+		dest[len + i] = *src;
+		src++;
+	}
+	dest[len + i] = '\0';
+	return (dest);
+}
---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*_abs - computes the absolute value of an number
+ *
+*@n: function parameter
+ *
+*Return: n or -n;
+ *
+ */
+int _abs(int n)
+
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}

144b2a3053e9acbe8b1d491643d052817425cfcd
Shatudanjuma <maryaishatudanjuma@gmail.com>
2023-08-17 07:50:59 -0700

    2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -14,7 +14,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	unsigned int index;
 
 	va_start(strings, n);
-	for (index = 0; index < n; index++)
+		for (index = 0; index < n; index++)
 		{
 			str = va_arg(strings, char *);
 			if (str == NULL)
